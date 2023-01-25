a54eab0146eb4dfb2045bf2f7a1010b0b5eb90a4
Eridiant <eridiante@gmail.com>
2023-01-25 16:36:35 +0300

    add all

--- main.js
+++ main.js
@@ -1,48 +1,78 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll(".square");
+    const square = document.querySelector(".square-wrapper");
+    const errors = document.querySelector("#errors");
+    const err = document.querySelector("#error");
+    const info = document.querySelector("#info");
+
+    let r, g, b;
+    let error = 0;
+    let differentSquare;
+    let different = 0;
+
+    changeColor();
 
     // generate random color for each square
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = randomColor();
+    // for (let i = 0; i < squares.length; i++) {
+    //     squares[i].style.backgroundColor = randomColor();
+    // }
+
+    function changeColor() {
+        let rndColor = randomColor()
+        square.style.backgroundColor = rndColor;
+
+        // select one square to be slightly different
+        differentSquare = squares[Math.floor(Math.random() * squares.length)];
+        differentSquare.style.backgroundColor = slightlyDifferentColor(rndColor);
     }
 
-    // select one square to be slightly different
-    let differentSquare = squares[Math.floor(Math.random() * squares.length)];
-    differentSquare.style.backgroundColor = slightlyDifferentColor(differentSquare.style.backgroundColor);
+    // add event listener
+    square.addEventListener("click", listener);
+    function listener(e) {
+        let t = e.target;
 
-    // add event listener to each square
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("click", function () {
-            if (this === differentSquare) {
-                alert("You found the different square!");
-                reduceDifference();
-                location.reload();
-            } else {
-                alert("Try again!");
-            }
-        });
+        if (t === differentSquare) {
+            err.removeAttribute("class");
+            err.classList.add('true');
+            reduceDifference();
+            changeColor();
+        } else {
+            err.removeAttribute("class");
+            err.classList.add('false');
+            error++;
+            errors.innerHTML = error;
+        }
+        if (error > 5) {
+            square.removeEventListener('click', listener);
+            info.innerHTML = `результат ${different}/50`
+        };
+        if (different > 49) {
+            square.removeEventListener('click', listener);
+            alert('victory')
+        };
     }
 
     // function to generate random color
     function randomColor() {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        return "rgb(" + r + ", " + g + ", " + b + ")";
+        r = Math.floor(Math.random() * 256);
+        g = Math.floor(Math.random() * 256);
+        b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
     }
 
     // function to generate slightly different color
     function slightlyDifferentColor(color) {
-        let newColor = color.slice(4, -1).split(", ");
-        newColor[0] = parseInt(newColor[0]) + 50;
-        newColor[1] = parseInt(newColor[1]) + 50;
-        newColor[2] = parseInt(newColor[2]) + 50;
-        return "rgb(" + newColor[0] + ", " + newColor[1] + ", " + newColor[2] + ")";
+        r = r + (50 - different);
+        g = g + (50 - different);
+        b = b + (50 - different);
+        if (different < 25) different += 2;
+        else different++
+        return `rgb(${r}, ${g}, ${b})`;
     }
 
     // function to reduce difference in color
     function reduceDifference() {
-        differentSquare.style.backgroundColor = randomColor();
+        differentSquare.style.cssText = '';
     }
 
 })
